cdb23d659d247ef91680a6b121e6d52e272a02d5
auliafriska926com <165472953+auliafriska926com@users.noreply.github.com>
2024-03-30 19:48:25 +0700

    first commit

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,12 @@
+import { BeakerIcon } from '@heroicons/react/24/outline';
+
+
+export default function AcmeLogo() {
+  return (
+    <div
+    >
+      <BeakerIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]"> Acme </p>
+    </div>
+  );
+}
