a7defb27fdac846cc4213391e9436029bcde8210
auliafriska926com <165472953+auliafriska926com@users.noreply.github.com>
2024-03-31 22:01:16 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+        < AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -22,12 +22,12 @@ export default function Page() {
         <div className={styles.shape}></div>
  
           
-        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome Frisca's Web.</strong> This is the excample for the{' '}
+        <p className={`${lusitana.className} text-xl text-gray-600 md:text-2xl md:leading-normal`}>
+            <strong>Welcome to Frisca's Web.</strong> This website was created by Frisca using{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Frisca.
+            , brought to you by Vercel.
           </p>
           <h1 className="text-blue-800">let's get it!</h1>
           <Link
